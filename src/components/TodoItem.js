40c8a2b94fb5c8ad22e6bbcdb1ec3bdd9541adc7
LichtLights <rlatjdwns151@outlook.com>
2023-04-11 13:22:52 +0900

    commit

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -8,7 +8,7 @@ import React from "react";
 import styles from "@/styles/TodoList.module.css";
 
 // TodoItem 컴포넌트를 정의합니다.
-const TodoItem = ({ todo, onToggle, onDelete }) => {
+const TodoItem = ({ todo, onToggle, todoDate, onDelete }) => {
   // 각 할 일 항목을 렌더링합니다.
   return (
     <li className={styles.todoItem}>
@@ -23,6 +23,13 @@ const TodoItem = ({ todo, onToggle, onDelete }) => {
       >
         {todo.text}
       </span>
+
+      {/* <span
+        className={styles.todoText}
+        style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+      >
+        {todo.todoDate}
+      </span> */}
 
       {/* 삭제 버튼을 렌더링하고, 클릭 시 onDelete 함수를 호출하여 해당 할 일을 삭제합니다. */}
       <button onClick={onDelete}>Delete</button>
